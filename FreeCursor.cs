1741d3875857fdf9949a1602526226faac904630
William Kuzniarz <wkhappiness@gmail.com>
2024-06-28 08:24:16 -0400

    Added FreeCursor.cs

---
+++ FreeCursor.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace ObraDinnArchipelago;
+
+[HarmonyPatch]
+public class FreeCursor
+{
+    public static bool debug = false;
+
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(RInput), "UpdateMousePosition")]
+    public static bool RInput_UpdateMousePosition_Prefix(bool appHasFocus,
+        Vector2 ___mousePosition_)
+    {
+        if (debug)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            return false;
+        }
+        else return true;
+
+    }
+}
--- Plugin.cs
+++ Plugin.cs
@@ -1,5 +1,8 @@
-﻿using BepInEx;
+﻿using System.Reflection;
+using BepInEx;
 using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
 
 namespace ObraDinnArchipelago;
 
@@ -13,5 +16,12 @@ public class Plugin : BaseUnityPlugin
         // Plugin startup logic
         Logger = base.Logger;
         Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+    }
+    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F12))
+            FreeCursor.debug = !FreeCursor.debug;
     }
 }
