edc5fd63055ffcc987b30648ee02cf3f6068ee58
blairbobaggins <48497324+blairbobaggins@users.noreply.github.com>
2024-07-03 15:38:05 -0400

    Fixed Skip Tutorial.
    Did not remove comments

--- patches/Startup.cs
+++ patches/Startup.cs
@@ -14,19 +14,26 @@ public class Startup
         SaveData.it.DebugGive();
         SaveData.it.DebugVisitAllMoments();
         SaveData.it.SetPlayerExploringSpot(new Player.Spot(new Vector3(-4.64f, 0.9f, 4.14f), Quaternion.Euler(0, 180.7f, 0)));
-        Game.LoadStartingShip();
-        return false;
-    }
-    [HarmonyPatch(typeof(BookTut), nameof(BookTut.Start), MethodType.Normal)]
-    [HarmonyPrefix]
-    static bool CutTutorial()
-    {
         SaveData.it.general.helpedBookBookmarks = true;
         SaveData.it.general.helpedBookDifficulty = true;
         SaveData.it.general.helpedBookFaceBlur = true;
         SaveData.it.general.helpedBookFaceClear = true;
         SaveData.it.general.helpedBookFatesCheck = true;
         SaveData.it.general.helpedBookUsage = true;
+        Game.LoadStartingShip();
+        
         return false;
     }
+    //[HarmonyPatch(typeof(BookTut), nameof(BookTut.Start), MethodType.Normal)]
+    //[HarmonyPrefix]
+    //static bool CutTutorial()
+    //{
+    //    SaveData.it.general.helpedBookBookmarks = true;
+    //    SaveData.it.general.helpedBookDifficulty = true;
+    //    SaveData.it.general.helpedBookFaceBlur = true;
+    //    SaveData.it.general.helpedBookFaceClear = true;
+    //    SaveData.it.general.helpedBookFatesCheck = true;
+    //    SaveData.it.general.helpedBookUsage = true;
+    //    return false;
+    //}
 }
